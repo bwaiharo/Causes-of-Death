72784e541d748b54724c6ba59720a81f5f1194ca
Benedict Waiharo <benabana@live.com>
2019-12-17 21:01:34 -0500

    Mongo data added

--- pymongo_etl.py
+++ pymongo_etl.py
@@ -1,33 +1,35 @@
 import json
-my_list = []
+import csv
+import pymongo
+death_list = []
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+# Define the 'classDB' database in Mongo
+db = client.NYData
+
+with open(r'C:\Users\benab\OneDrive\Documents\Bootcampsot\Causes-of-Death\NY_clean_data.json', 'r',encoding = 'utf-8') as data_file:
+    data = data_file.read()
+    obj = json.loads(data)
+    print(obj["columns"])
+    columns = obj["columns"]
+    # print(obj["data"])
+    for row in obj["data"]:
+        count = 0
+        finalrow = {}
+        for d in row:
+            print(d)
+            print(columns[count])
+            finalrow[columns[count]] = d
+            count += 1
+        db.ny_death.insert_one(finalrow)
+        
+
+# db.NYJson.insert_many(death_list).inserted_ids
 
 
-import os
 
-import re
 
-text_file = os.path.join(r'Causes-of-Death\NY_death_data.txt')
-
-with open (text_file, "r",encoding = 'utf-8') as f:
-
-    lines = f.readlines() # list containing lines of file
-    print(lines)
-#     columns = [] # To store column names
-
-#     i = 1
-#     for line in lines:
-#         line = line.strip() # remove leading/trailing white spaces
-#         if line:
-#             if i == 1:
-#                 columns = [item.strip() for item in line.split(' ')]
-#                 i = i + 1
-#             else:
-#                 d = {} # dictionary to store file data (each line)
-#                 data = [item.strip() for item in line.split(' ')]
-#                 for index, elem in enumerate(data):
-#                     d[columns[index]] = data[index]
-
-#                 my_list.append(d) # append dictionary to list
-
-# # pretty printing list of dictionaries
-# print(json.dumps(my_list, indent=4))
+# nydeath = db.NYJson.find()
+# for student in nydeath:
+#     print(student)
